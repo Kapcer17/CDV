702d8c781d086af426d16a65302b23a7e3462a13
Kapcer17 <95164009+Kapcer17@users.noreply.github.com>
2025-04-03 01:16:02 +0200

    Add files via upload
    OOP Exercises from lecture 4

---
+++ Programowanie obiektowe/Programowanie obiektowe/Zajecia4/TypyGeneryczne/TypyGeneryczne/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic; // Konieczne dla Stack<T>
+using System.Collections.Immutable;
+
+namespace typy_generyczne
+{
+    class Person : IComparable
+    {
+        private String name, lastname;
+        private long PESEL;
+
+        public Person(String _name, String _lastname, long _PESEL)
+        {
+            name = _name;
+            lastname = _lastname;
+            PESEL = _PESEL;
+        }
+
+        public override string ToString()
+        {
+            return "Imię: " + name + "\r\n" + "Nazwisko: " + lastname + "\r\n"
+            + "PESEL: " + PESEL.ToString() + "\r\n";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Person p = obj as Person;
+            if (p != null)
+            {
+                if(p.lastname.CompareTo(lastname) == 0)
+                {
+                    return p.lastname.CompareTo(name);
+                }
+                return p.lastname.CompareTo(lastname);
+            }
+            return 0;
+        }
+    }
+    public class Stack<T> where T : IComparable
+    {
+        private T[] stackT = null;
+        private int i = 0;
+
+        public Stack(int _size)
+        {
+            stackT = new T[_size];
+        }
+
+        public void Push(T element)
+        {
+            if (i < stackT.Length)
+            {
+                stackT[i] = element;
+                i++;
+            }
+        }
+
+        public T Pop()
+        {
+            if (i == 0)
+            {
+                return default(T);
+            }
+            i--;
+            return stackT[i];
+        }
+
+        public T Peek()
+        {
+            if (i == 0)
+            {
+                return default(T);
+            }
+            return stackT[i - 1];
+        }
+
+        public bool isEmpty()
+        {
+            if (i == 0)
+            {  
+                return true; 
+            }
+            return false;
+        }
+
+        public void Sort()
+        {
+            Array.Sort(stackT, 0, i);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Person> persons = new Stack<Person>(10);
+            persons.Push(new Person("Jan", "Kowalski", 45081188771));
+            persons.Push(new Person("Maciej", "Ziółowski", 11112225789));
+            persons.Push(new Person("Karolina", "Nowak", 12345678910));
+
+            Console.WriteLine(persons.Pop());
+            Console.WriteLine(persons.Pop());
+            Console.WriteLine(persons.Pop());
+
+            persons.Push(new Person("Jan", "Kowalski", 45081188771));
+            persons.Push(new Person("Maciej", "Ziółowski", 11112225789));
+            persons.Push(new Person("Karolina", "Nowak", 12345678910));
+
+            persons.Sort();
+            Console.WriteLine(persons.Pop());
+            Console.WriteLine(persons.Pop());
+            Console.WriteLine(persons.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Programowanie obiektowe/Programowanie obiektowe/Zajecia4/TypyGeneryczneFIFO/TypyGeneryczneFIFO/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace typy_generyczne
+{
+    class Person : IComparable
+    {
+        private string name, lastname;
+        private long PESEL;
+
+        public Person(string _name, string _lastname, long _PESEL)
+        {
+            name = _name;
+            lastname = _lastname;
+            PESEL = _PESEL;
+        }
+
+        public override string ToString()
+        {
+            return $"Imię: {name}\r\nNazwisko: {lastname}\r\nPESEL: {PESEL}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            Person p = obj as Person;
+            if (p != null)
+            {
+                if (lastname.CompareTo(p.lastname) == 0)
+                {
+                    return name.CompareTo(p.name);
+                }
+                return lastname.CompareTo(p.lastname);
+            }
+            return 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person p)
+            {
+                return this.name == p.name && this.lastname == p.lastname && this.PESEL == p.PESEL;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (name + lastname + PESEL).GetHashCode();
+        }
+    }
+
+
+    public class Queue<T> where T : IComparable
+    {
+        private LinkedList<T> queue = new LinkedList<T>();
+
+        public void Enqueue(T element)
+        {
+            queue.AddLast(element);
+        }
+
+        public T Dequeue()
+        {
+            if (queue.Count == 0)
+            {
+                return default(T);
+            }
+            T value = queue.First.Value;
+            queue.RemoveFirst();
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (queue.Count == 0)
+            {
+                return default(T);
+            }
+            return queue.First.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return queue.Count == 0;
+        }
+
+        public bool FindElement(T element)
+        {
+            return queue.Contains(element);
+        }
+
+        public void ForEachElement(Action<T> action)
+        {
+            foreach (T item in queue)
+            {
+                action(item);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Person> personsQueue = new Queue<Person>();
+
+            personsQueue.Enqueue(new Person("Jan", "Kowalski", 45081188771));
+            personsQueue.Enqueue(new Person("Maciej", "Ziółowski", 11112225789));
+            personsQueue.Enqueue(new Person("Karolina", "Nowak", 12345678910));
+
+            Console.WriteLine("Dequeue elements:");
+            Console.WriteLine(personsQueue.Dequeue());
+            Console.WriteLine(personsQueue.Dequeue());
+            Console.WriteLine(personsQueue.Dequeue());
+
+            personsQueue.Enqueue(new Person("Jan", "Kowalski", 45081188771));
+            personsQueue.Enqueue(new Person("Maciej", "Ziółowski", 11112225789));
+            personsQueue.Enqueue(new Person("Karolina", "Nowak", 12345678910));
+
+            bool exists = personsQueue.FindElement(new Person("Jan", "Kowalski", 45081188771));
+            Console.WriteLine($"\nWyszukanie czy Jan Kowalski jest w kolejce - {exists}");
+
+            Console.WriteLine("\nIterowanie:");
+            personsQueue.ForEachElement(person => Console.WriteLine(person));
+
+            Console.ReadKey();
+        }
+    }
+}
